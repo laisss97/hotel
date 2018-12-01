4aeeac96b42c1e8422a5f67a3b7ac492945b470d
isadora23 <isadora.martinez23@gmail.com>
2018-12-01 19:19:11 -0300

    Pequenos erros corrigidos

--- view/quartos.php
+++ view/quartos.php
@@ -49,18 +49,18 @@ Data: 11 /10 /2018
 	<div class="quartos">
 		
 		<img src="http://www.sw-hotelguide.com/portugal/bussaco/palace-hotel-bussaco/images/stories/rotators/rooms/rooms_02.jpg"class="img-rounded" alt="quarto1"  width=60%; height=70%;>
-		<div class="descricao"><strong>Quarto Simple:</strong> cama king-size, ar-condicionado, wi-fi, tv e decoração em tons quentes.</div>
+		<div class="descricao"><strong>Quarto Simplex:</strong> cama king-size, ar-condicionado, wi-fi, tv e decoração em tons quentes.</div>
 
 		<img src="http://s2.glbimg.com/QfxKhkTZyfQ7LgRKIi6jG1jDIog=/s.glbimg.com/jo/g1/f/original/2013/10/04/img_3116.jpg" class="img-rounded" alt="quarto2" width=60%; height=60%;>
-		<div class="descricao"><strong>Quarto Lux:</strong> cama king-size, ar-condicionado, wi-fi, smart-tv, pequeno escritório adjunto para reuniõs, minibar e decoração em tons pastéis.</div>
+		<div class="descricao"><strong>Quarto Lux:</strong> cama king-size, ar-condicionado, wi-fi, smart-tv, pequeno escritório adjunto para reuniões, minibar e decoração em tons pastéis.</div>
 
 
 		<img src="https://st2.depositphotos.com/1006832/7530/i/950/depositphotos_75307465-stock-photo-luxury-hotel-bedroom.jpg" class="img-rounded" alt="quarto3" width=60%; height=60%;>
-		<div class="descricao"><strong>Quarto Lux Master:</strong> cama king-size, ar-condicionado, wi-fi, smart-tv, pequeno escritório adjunto para reuniõs, frigorífico, minibar e decoração em tons claros.</div>
+		<div class="descricao"><strong>Quarto Lux Master:</strong> cama king-size, ar-condicionado, wi-fi, smart-tv, pequeno escritório adjunto para reuniões, frigorífico, minibar e decoração em tons claros.</div>
 
 
 		<img src="http://www.webluxo.com.br/menu/hoteis/2009/mardan-palace_hotel-7.jpg" class="img-rounded" alt="quarto4" width=60%; height=60%;>
-		<div class="descricao"><strong>Quarto Lux Imperial:</strong> cama king-size, ar-condicionado, wi-fi, smart-tv, pequeno escritório adjunto para reuniõs, frigorífico, minibar, visão panorâmica da cidade e decoração em tons escuros.</div>
+		<div class="descricao"><strong>Quarto Lux Imperial:</strong> cama king-size, ar-condicionado, wi-fi, smart-tv, pequeno escritório adjunto para reuniões, frigorífico, minibar, visão panorâmica da cidade e decoração em tons escuros.</div>
 
 
 	</div>
