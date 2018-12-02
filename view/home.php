f4253da45e265bac133062e646592cf896df6c3e
isadora23 <isadora.martinez23@gmail.com>
2018-12-01 22:46:49 -0300

    Acrescentada uma galeria com imagens dos ambientes principais do hotel

--- view/home.php
+++ view/home.php
@@ -31,6 +31,57 @@ Data: 11 /10 /2018
 	<article>
 		<h2>Bem vindo!</h2>
 		<p>O Hotel Palácios está há 200 anos oferecendo hospedagem, conforto e requinte para seus hóspedes.</p>
+		
+		<!--Galeria de imagens internas do hotel-->
+
+		<div class="gallery">
+			<a target="_blank" href="https://media-cdn.tripadvisor.com/media/photo-s/0a/63/9c/f4/recepcao.jpg">
+				<img src="https://media-cdn.tripadvisor.com/media/photo-s/0a/63/9c/f4/recepcao.jpg" alt="recepcao" width="200" height="200">
+			</a>
+			<div class="desc">Recepção</div>
+		</div>
+
+		<div class="gallery">
+			<a target="_blank" href="http://www.sw-hotelguide.com/portugal/bussaco/palace-hotel-bussaco/images/stories/rotators/rooms/rooms_02.jpg">
+				<img src="http://www.sw-hotelguide.com/portugal/bussaco/palace-hotel-bussaco/images/stories/rotators/rooms/rooms_02.jpg" alt="quarto" width="200" height="200">
+			</a>
+			<div class="desc">Quarto</div>
+		</div>
+
+		<div class="gallery">
+			<a target="_blank" href="http://www.hotelestanciaatibainha.com.br/images/lazer/piscinas/coberta-infantil-2.jpg">
+				<img src="http://www.hotelestanciaatibainha.com.br/images/lazer/piscinas/coberta-infantil-2.jpg" alt="piscina" width="200" height="200">
+			</a>
+			<div class="desc">Piscina</div>
+		</div>
+
+		<div class="gallery">
+			<a target="_blank" href="http://www.hotelunique.com.br/images/034_hotel%20unique_%20spa%20room.jpg?crc=319208505">
+				<img src="http://www.hotelunique.com.br/images/034_hotel%20unique_%20spa%20room.jpg?crc=319208505" alt="spa" width="200" height="200">
+			</a>
+			<div class="desc">Spa</div>
+		</div>
+
+		<div class="gallery">
+			<a target="_blank" href="http://www.sw-hotelguide.com/portugal/bussaco/palace-hotel-bussaco/images/stories/rotators/restaurants/restaurants_03.jpg">
+				<img src="http://www.sw-hotelguide.com/portugal/bussaco/palace-hotel-bussaco/images/stories/rotators/restaurants/restaurants_03.jpg" alt="salão de festas" width="200" height="200">
+			</a>
+			<div class="desc">Salão de Festas</div>
+		</div>
+
+		<div class="gallery">
+			<a target="_blank" href="http://apureguria.com/wp-content/uploads/2016/02/academia-hotel-alpestre.jpg">
+				<img src="http://apureguria.com/wp-content/uploads/2016/02/academia-hotel-alpestre.jpg" alt="academia" width="200" height="200">
+			</a>
+			<div class="desc">Academia</div>
+		</div>
+
+		<div class="gallery">
+			<a target="_blank" href="https://gustavosirelli.files.wordpress.com/2010/04/republica.jpg">
+				<img src="https://gustavosirelli.files.wordpress.com/2010/04/republica.jpg" alt="jardim" width="200" height="200">
+			</a>
+			<div class="desc">Jardim</div>
+		</div>
 	</article>
 
 	<footer> 
